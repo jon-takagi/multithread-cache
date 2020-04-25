9afd52e1dd0a1407494fb616c165a17556e85e17
Jon Takagi <jontakagi@reed.edu>
2020-04-25 21:53:41 +0000

    ???

--- thread_test.cc
+++ thread_test.cc
@@ -17,7 +17,7 @@ void warm(Generator gen_, Cache* cache_, int size)
             std::string val_str = std::string(req.val_size_, 'B');
             //Cache::val_type val = val_str.c_str();
             char* val = new char [req.val_size_+1];
-            strcpy(val, val_str.c_str());
+            strcpy (val, val_str.c_str());
             cache_->set(req.key_, val, req.val_size_+1);
             delete[] val;
             sets += 1;
@@ -44,7 +44,6 @@ Numeric random(Numeric from, Numeric to)
 
 
 double time_single_request(Generator gen_, Cache* cache_) {
-    std::cout << "thread " << std::this_thread::get_id() << " running" << std::endl;
     std::chrono::time_point<std::chrono::high_resolution_clock> t1;
     std::chrono::time_point<std::chrono::high_resolution_clock> t2;
     Request req = gen_.gen_req(false);
@@ -67,42 +66,37 @@ double time_single_request(Generator gen_, Cache* cache_) {
     // std::cout << std::get<2>(req) << " [key: " << std::get<0>(req) << ", val: " << std::get<1>(req) <<"]"<< std::endl;
         t2 = std::chrono::high_resolution_clock::now();
     }
-    std::cout << std::endl;
     std::chrono::duration<double, std::milli> elapsed = std::chrono::duration_cast<std::chrono::duration<double, std::milli>> (t2-t1);
     return elapsed.count();
 }
 
 void do_nreq_requests(Generator gen_, Cache* cache_, int nreq, std::promise<std::vector<double>> *promObj)
 {
-    std::cout << "timing " << nreq << " requests" << std::endl;
     std::vector<double> results(nreq, -1.0);
     for(int i = 0; i < nreq; i++ ) {
         results[i] = time_single_request(gen_, cache_);
-        // if(i % (nreq / 100) == 0) {
-        //     std::cout << ".";
-        // }
     }
-    std::cout << "thread " << std::this_thread::get_id() << " done" << std::endl;
     promObj->set_value(results);
 }
 
 int main()
 {
     const int CACHE_SIZE = 8192;
-    const int TRIALS = 1000;
-    const int THREADS = 8;
+    const int TRIALS = 10;
+    const int THREADS = 2;
     Generator gen = Generator(8, 0.2, CACHE_SIZE, 8);
-    auto test_cache = Cache("127.0.0.1", "42069");
-
-    warm(gen, &test_cache, CACHE_SIZE);
 
     std::vector<std::thread> threads;
+    std::vector<Cache*> clients(THREADS, 0x0);
     std::vector<std::promise<std::vector<double>>> promises(THREADS);
     std::vector<std::future<std::vector<double>>> futures(THREADS);
     std::vector<std::vector<double>> results(THREADS, std::vector<double>(TRIALS));
     for(int i = 0; i < THREADS; i++){
+        Cache client = Cache("127.0.0.1", "42069");
+        clients[i] = &client;
+        warm(gen, clients[i], CACHE_SIZE);
         futures[i] = promises[i].get_future();
-        threads.push_back(std::thread(do_nreq_requests, gen, &test_cache, TRIALS, &(promises[i])));
+        threads.push_back(std::thread(do_nreq_requests, gen, clients[i], TRIALS, &(promises[i])));
     }
     for(int i = 0; i < THREADS; i++) {
         threads[i].join();
